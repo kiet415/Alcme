a3651a9a03cd6fc82d65aaae7939f3f66f941785
brassmacks <jadepavedpath@gmail.com>
2021-08-16 11:41:11 -0700

    'layout for models, routes'

---
+++ models/Ingredient.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoos.Schema;
+
+const IngredientSchmema = new Schema({
+  // id => integer
+  // original == name => string
+  // aisle =>string
+  //image url => string
+  // nutrition obj => {
+  //  nutrients => []
+  // }
+
+})
