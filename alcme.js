b9babe5618b3eebe9cac5ff0ca1dd67ebb4b8ee2
brassmacks <jadepavedpath@gmail.com>
2021-08-16 01:00:03 -0700

    initialized project, started api paths, auth

---
+++ alcme.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+const db = require('./config/keys').mongoURI;
+const passport = require('passport');
+const users = require('./models/User')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// check deprecation status of bodyParser
+// preceding lines could be outdated
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  // added useUnifiedTopology as a fix for deprecation warnings
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use("/api/users", users);
+
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
