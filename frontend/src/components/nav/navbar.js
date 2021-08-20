e5a82ea099c683238ebfe553ac759d16f7e55548
lbrilli <lbrilli11@gmail.com>
2021-08-19 18:16:46 -0600

    fix recipe showing only when logged in

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -15,18 +15,14 @@ class NavBar extends React.Component {
   getLinks() {
       if (this.props.loggedIn) {
         return (
-            <div>
+            <div className="buttons-logged-in">
+                <Link to="/recipe/create" className="ingredient-index-button">Create Recipe</Link>
                 <button onClick={this.logoutUser} className="logout-button">Logout</button>
             </div>
         );
       } else {
         return (
             <div>
-              <a href="http://localhost:3000/#/">
-                <img src="ALcme.png"></img>
-              </a>
-              <br/>
-              <Link to="/recipe/create" className="ingredient-index-button">Create Recipe</Link>
               <br/>
               <Link to="/recipes" className="ingredient-index-button">Recipe index page</Link>
               <Link to={'/signup'} className="signup-button">Signup</Link>
@@ -39,6 +35,9 @@ class NavBar extends React.Component {
   render() {
       return (
         <div className="navbar-head">
+              <a href="http://localhost:3000/#/">
+                <img src="ALcme.png"></img>
+              </a>
             { this.getLinks() }
         </div>
       );
